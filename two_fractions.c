fcee950a814ef3d77564584c15c0a3340e9bc0d8
ENG19CS0036_ANJANA KP <76141960+ENG19CS0036Anjanakp@users.noreply.github.com>
2021-02-04 13:32:26 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -1,1 +1,11 @@
-//WAP to find the sum of two fractions.
+//WAP to find the sum of two fractions.#include<stdio.h>
+int main()
+{
+int n1,n2,sum;
+printf("enter the two numbers :");
+scanf("%d%d",&n1,&n2);
+sum=n1+n2;
+printf("Sum of two numbers %d+%d=%d",n1,n2,sum);
+return 0;
+}
+
